09249f947f48cb20b6ae83a406f9255eb0c2e0f2
benjaminMWhite <bwhitem@gmail.com>
2022-03-10 13:46:00 -0500

    getting handlebars

--- public/js/db_work.js
+++ public/js/db_work.js
@@ -1,4 +1,4 @@
-const { json } = require("express/lib/response")
+//const { json} = require("express/lib/response")
 
 var dbBuild = "https://api.jikan.moe/v4/anime/"+id+"/episodes/"+episode
 
@@ -7,13 +7,23 @@ var animeSeries =["naruto","naruto Shippuden"]
 var animeID = ["20","1735"]
 var animeEpCt = ["220","500"]
 
-for (let k=0 ;k<animeID.length; k++  )
-{ var id = animeID[k]
-    for (let i=0; i < animeEpCt[k]; i++)
-{ var episode = i
+
+function getInfo(){
+//for (let k=0 ;k<animeID.length; k++  )
+//{ var id = animeID[k]
+  //  for (let i=0; i < 1 ; i++)
+//{
+   var episode = 1
+  var id = 20  
     sleep(1001)
     fetch (dbBuild)
     .then(function(response)
     {console.log(response)
+        
     return response.json()})
-}}
+//}
+}
+//}
+
+getInfo()
+// animeEpCt[k]
