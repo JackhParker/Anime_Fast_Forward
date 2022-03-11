bf9c735821c3cae3d21bfc3976004f9a8230cb6b
benjaminMWhite <bwhitem@gmail.com>
2022-03-11 12:08:42 -0500

    models

---
+++ controllers/api/dbroutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { SpecificEpisode } = require('../../models');
+
+router.post('/info', async (req, res) => {
+  try {
+      console.log(req.body)
+    const episode = await SpecificEpisode.create({...req.body } );
+
+   res.status(200);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+})
+
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-
+const dbroutes= require("./dbroutes");
 router.use('/users', userRoutes);
-
+router.use('/database', dbroutes);
 module.exports = router;
